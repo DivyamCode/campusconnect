0d914c5e5a9d4a3598afe0b928c6b72fd287dfdc
Dhairya <dhairyaseth30@gmail.com>
2023-04-27 21:30:39 +0530

    configSignup

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,88 @@
-<<<<<<< HEAD
+// import 'package:campusconnect/screen/rollno.dart';
+// import 'package:campusconnect/screen/signup.dart';
+// import 'package:flutter/material.dart';
+// import 'package:provider/provider.dart';
+// // import 'package:screen/screen.dart';
+// import 'package:campusconnect/provider/themeprovider.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MultiProvider(
+//       providers: [
+//         ChangeNotifierProvider(
+//           create: (context) => ThemeProvider(),
+//         ),
+//       ],
+//       child: MaterialApp(
+//         debugShowCheckedModeBanner: false,
+//           title: 'Flutter Demo',
+//           theme: ThemeData(
+//             primarySwatch: Colors.blue,
+//           ),
+//           home: Divyam(),
+//           routes: {
+//             "signup":(context) => Signup(),
+//             "rollno":(context) => Rollno(),
+//           },
+//           ),
+//     );
+//   }
+// }
+
+
+
+// class Divyam extends StatefulWidget {
+//   const Divyam({Key? key}) : super(key: key);
+
+//   @override
+//   _DivyamState createState() => _DivyamState();
+// }
+
+// class _DivyamState extends State<Divyam> {
+//   @override
+//   Widget build(BuildContext context) {
+//     final themeProvoder = Provider.of<ThemeProvider>(context);
+
+//     return Material(
+//       child: Container(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [InkWell(
+//             onTap: () {
+//               Navigator.of(context).pushNamed("signup");
+//             },
+//             child: Container(
+//               decoration: BoxDecoration(
+//                 color: themeProvoder.getPrimaryColor,
+//                 border: Border.all(color: Colors.black26),
+//                 borderRadius: BorderRadius.circular(15),
+//               ),
+//               child: Text("Signup"),
+//             ),
+//           )],
+//         ),
+//         // color: themeProvoder.getPrimaryColor,
+//       ),
+//     );
+//   }
+// }
+
+
+
+
 import 'package:campusconnect/screen/Welcome.dart';
 import 'package:campusconnect/screen/onboarding.dart';
-import 'package:campusconnect/screen/sign.dart';
-=======
 import 'package:campusconnect/screen/rollno.dart';
+import 'package:campusconnect/screen/sign.dart';
 import 'package:campusconnect/screen/signup.dart';
->>>>>>> 7bf7ee89348ee9e12e59b28e010c989ef1c373de
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 // import 'package:screen/screen.dart';
@@ -28,39 +105,24 @@ class MyApp extends StatelessWidget {
         ),
       ],
       child: MaterialApp(
-<<<<<<< HEAD
+        debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
         home: Divyam(),
-        routes: {"AppOpenroute": (context) => AppOpen(),
-          "Welcome":(context) => dhairya(),
-          "sign":(context)=> harshit(),
+        routes: {
+          "AppOpenroute": (context) => AppOpen(),
+          "Welcome": (context) => dhairya(),
+          "sign": (context) => harshit(),
+          "signup": (context) => Signup(),
+          "rollno": (context) => Rollno(),
         },
       ),
-=======
-        debugShowCheckedModeBanner: false,
-          title: 'Flutter Demo',
-          theme: ThemeData(
-            primarySwatch: Colors.blue,
-          ),
-          home: Divyam(),
-          routes: {
-            "signup":(context) => Signup(),
-            "rollno":(context) => Rollno(),
-          },
-          ),
->>>>>>> 7bf7ee89348ee9e12e59b28e010c989ef1c373de
     );
   }
 }
 
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 7bf7ee89348ee9e12e59b28e010c989ef1c373de
 class Divyam extends StatefulWidget {
   const Divyam({Key? key}) : super(key: key);
 
@@ -74,38 +136,34 @@ class _DivyamState extends State<Divyam> {
     final themeProvoder = Provider.of<ThemeProvider>(context);
 
     return Material(
-<<<<<<< HEAD
       child: Center(
-          child: InkWell(
-        child: Text("AppOpen"),
-        onTap: () {
-          Navigator.pushNamed(context, "AppOpenroute");
-        },
-      ),
+        child:SafeArea(
+          child: Column(
+            children: [
+              InkWell(
+                child: Text("AppOpen"),
+                onTap: () {
+                  Navigator.pushNamed(context, "AppOpenroute");
+                },
+              ),
+              InkWell(
+                onTap: () {
+                  Navigator.of(context).pushNamed("signup");
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: themeProvoder.getPrimaryColor,
+                    border: Border.all(color: Colors.black26),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Text("Signup"),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
       color: themeProvoder.getPrimaryColor,
-=======
-      child: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [InkWell(
-            onTap: () {
-              Navigator.of(context).pushNamed("signup");
-            },
-            child: Container(
-              decoration: BoxDecoration(
-                color: themeProvoder.getPrimaryColor,
-                border: Border.all(color: Colors.black26),
-                borderRadius: BorderRadius.circular(15),
-              ),
-              child: Text("Signup"),
-            ),
-          )],
-        ),
-        // color: themeProvoder.getPrimaryColor,
-      ),
->>>>>>> 7bf7ee89348ee9e12e59b28e010c989ef1c373de
     );
   }
 }
-
--- lib/screen/rollno.dart
+++ lib/screen/rollno.dart
@@ -12,14 +12,30 @@ class Rollno extends StatefulWidget {
 }
 
 class _RollnoState extends State<Rollno> {
+  TextEditingController rollController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
         child: Container(
+          padding: const EdgeInsets.only(left: 40, right: 40),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              const SizedBox(height: 5.0),
+              ListTile(
+                onTap: () {
+                  Navigator.of(context).pushNamed("signup");
+                },
+                leading: Icon(
+                  Icons.arrow_back_ios,
+                  color: Colors.black87,
+                  weight: 10.0,
+                ),
+              ),
+              const SizedBox(height: 30.0),
               Text(
                 "Enter your University Roll no",
                 style: TextStyle(
@@ -27,9 +43,37 @@ class _RollnoState extends State<Rollno> {
                     color: Colors.black87,
                   ),
                 ),
-
-                
-                
+                const SizedBox(height: 20.0),
+                TextField(
+                  keyboardType: TextInputType.number,
+                  controller: rollController,
+                  decoration: const InputDecoration(
+                    labelText: "Roll Number"
+                  ),
+                ),
+                const SizedBox(height: 60.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        // handle button press
+                      },
+                      child: Container(
+                        width: 56,
+                        height: 56,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.green.shade400,
+                        ),
+                        child: Icon(
+                          Icons.arrow_forward_ios,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
             ],
           ),
         ),
